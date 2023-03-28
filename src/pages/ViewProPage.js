d21b41da9a7401c3b209ef0819ef6b668006b1fc
ronish2722 <ronishshrestha2722@gmail.com>
2023-03-28 19:17:04 +0545

    only apporved professionals are displayed

--- src/pages/ViewProPage.js
+++ src/pages/ViewProPage.js
@@ -1,3 +1,64 @@
+// import React, { useEffect } from "react";
+// import Message from "../components/Message";
+// import Professionals from "../components/Professionals";
+// import Header from "../components/Header";
+// import { Loading } from "../components/Loading";
+// import { useDispatch, useSelector } from "react-redux";
+// import { listProfessionals } from "../actions/professionalAction";
+// import { Link } from "react-router-dom";
+
+// const ViewProPage = () => {
+//   // const [professionals, setProfessionals] = useState([]);
+
+//   const dispatch = useDispatch();
+//   const professionalList = useSelector((state) => state.professionalList);
+
+//   const { error, loading, professionals } = professionalList;
+
+//   useEffect(() => {
+//     // async function fetchProfessionals() {
+//     //   const { data } = await axios.get("/api/professionals/");
+//     //   setProfessionals(data);
+//     // }
+
+//     // fetchProfessionals();
+
+//     dispatch(listProfessionals());
+//   }, []);
+//   const approvedProfessionals = professionals.filter(
+//     (professional) => professional.is_approved
+//   );
+
+//   return;
+//   return (
+//     <div>
+//       <Header />
+//       <h1 className="font-black text-3xl pl-[50px] pt-[10px]">Professionals</h1>
+//       {loading ? (
+//         <Loading />
+//       ) : error ? (
+//         <Message message={error} type="error" />
+//       ) : (
+//         <div className="flex ">
+//           <div className="w-1/4 font-black text-xl pl-[50px] pt-[10px] border-r-2">
+//             Filters
+//           </div>
+
+//           <div className="mx-[50px]">
+//             {professionals.map((professional) => (
+//               <div key={professional._id}>
+//                 <Professionals professional={professional} />
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ViewProPage;
+
 import React, { useEffect } from "react";
 import Message from "../components/Message";
 import Professionals from "../components/Professionals";
@@ -8,23 +69,19 @@ import { listProfessionals } from "../actions/professionalAction";
 import { Link } from "react-router-dom";
 
 const ViewProPage = () => {
-  // const [professionals, setProfessionals] = useState([]);
-
   const dispatch = useDispatch();
   const professionalList = useSelector((state) => state.professionalList);
-
   const { error, loading, professionals } = professionalList;
 
   useEffect(() => {
-    // async function fetchProfessionals() {
-    //   const { data } = await axios.get("/api/professionals/");
-    //   setProfessionals(data);
-    // }
-
-    // fetchProfessionals();
-
     dispatch(listProfessionals());
   }, []);
+
+  // Filter the professionals by is_approved
+  const approvedProfessionals = professionals.filter(
+    (professional) => professional.is_approved
+  );
+
   return (
     <div>
       <Header />
@@ -38,8 +95,9 @@ const ViewProPage = () => {
           <div className="w-1/4 font-black text-xl pl-[50px] pt-[10px] border-r-2">
             Filters
           </div>
+
           <div className="mx-[50px]">
-            {professionals.map((professional) => (
+            {approvedProfessionals.map((professional) => (
               <div key={professional._id}>
                 <Professionals professional={professional} />
               </div>
